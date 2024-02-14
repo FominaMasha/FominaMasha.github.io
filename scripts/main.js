a34c9b559809ff63dc26c339bcb374c0d472db83
Фомина Мария <fomina_masha@list.ru>
2024-02-14 10:04:37 +0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#7C5BFF";
+
+let userName = document.getElementById("userName");
+
+userName = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+
 $(document).ready(function () {
-  let tg = window.Telegram.WebApp;
-
-  tg.expand();
-
-  tg.MainButton.textColor = "#FFFFFF";
-  tg.MainButton.color = "#7C5BFF";
-
-  let userName = document.getElementById("userName");
-
-  userName = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
-
   var acc = document.getElementsByClassName("accordion-item__header");
 
   for (var i = 0; i < acc.length; i++) {
